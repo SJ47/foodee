1294ca55414f97257ffdc5ac3611297f3d0a0c1a
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-04-08 18:07:41 +0100

    Data loader loaded with a bunch of data - customers, an order, some tables and menu items

--- foodee_service/src/main/java/com/ialcoholic/foodees/foodee_service/components/DataLoader.java
+++ foodee_service/src/main/java/com/ialcoholic/foodees/foodee_service/components/DataLoader.java
@@ -3,9 +3,15 @@ package com.ialcoholic.foodees.foodee_service.components;
 import com.ialcoholic.foodees.foodee_service.models.menu.Allergen;
 import com.ialcoholic.foodees.foodee_service.models.menu.Category;
 import com.ialcoholic.foodees.foodee_service.models.menu.MenuItem;
+import com.ialcoholic.foodees.foodee_service.models.orders.Order;
+import com.ialcoholic.foodees.foodee_service.models.people.Customer;
 import com.ialcoholic.foodees.foodee_service.models.restaurant.Restaurant;
+import com.ialcoholic.foodees.foodee_service.models.restaurant.Table;
 import com.ialcoholic.foodees.foodee_service.repositories.menu.MenuItemRepository;
+import com.ialcoholic.foodees.foodee_service.repositories.orders.OrderRepository;
+import com.ialcoholic.foodees.foodee_service.repositories.people.CustomerRepository;
 import com.ialcoholic.foodees.foodee_service.repositories.restaurant.RestaurantRepository;
+import com.ialcoholic.foodees.foodee_service.repositories.restaurant.TableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -15,16 +21,22 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements ApplicationRunner {
 
     @Autowired
-    MenuItemRepository menuItemRepository;
-
-
-    @Autowired
     RestaurantRepository restaurantRepository;
 
+    @Autowired
+    MenuItemRepository menuItemRepository;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    TableRepository tableRepository;
 
     public DataLoader() {
     }
-
 
     public void run(ApplicationArguments args) {
 
@@ -32,7 +44,7 @@ public class DataLoader implements ApplicationRunner {
         Restaurant restaurant = new Restaurant("Foodee");
         restaurantRepository.save(restaurant);
 
-        // Setup Category
+        // Setup categories
         Category child_meal = Category.CHILD_MEAL;
         Category main = Category.MAIN;
         Category side = Category.SIDE;
@@ -42,9 +54,14 @@ public class DataLoader implements ApplicationRunner {
         Category hard_drink = Category.HARD_DRINK;
         Category soft_drink = Category.SOFT_DRINK;
 
+        // Setup customers
+        Customer renee = new Customer("Renee", "Graham", "renee.graham@fakeemail.com");
+        Customer reneesHusband = new Customer("Mister", "Man", "mister.man@fakeemail.com");
+        customerRepository.save(renee);
+        customerRepository.save(reneesHusband);
 
-        // Setup order
         // Setup menu items
+        // KIDS //
         MenuItem menuItem1 = new MenuItem(restaurant, child_meal, "Cheese Sticks", "Old favourites made with love",
                 5.00, "https://images.unsplash.com/photo-1593895647433-5a608a3bd379?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2hlZXNlJTIwc3RpY2tzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
                 false, true, false, false, "100g",
@@ -74,6 +91,7 @@ public class DataLoader implements ApplicationRunner {
         menuItem3.addAllergen(Allergen.MILK);
         menuItemRepository.save(menuItem3);
 
+        // SIDES //
         MenuItem menuItem4 = new MenuItem(restaurant, side, "Potato fries", "Specially made from organic Scottish " +
                 "potatoes", 3.50, "https://images.unsplash.com/photo-1600339240932-226cff938b4f?ixlib=rb-1.2" +
                 ".1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80", true, true
@@ -90,6 +108,7 @@ public class DataLoader implements ApplicationRunner {
         menuItem5.addAllergen(Allergen.GLUTEN);
         menuItemRepository.save(menuItem5);
 
+        // SNACKS //
         MenuItem menuItem6 = new MenuItem(restaurant, snack, "Haggis Pakora", "Delicious fusion dish that combines a " +
                 "taste of Scotland with an Indian flare", 5.0, "https://images.unsplash" +
                 ".com/photo-1597559824960-df4d323435b9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1" +
@@ -122,6 +141,7 @@ public class DataLoader implements ApplicationRunner {
         menuItem8.addAllergen(Allergen.FISH);
         menuItemRepository.save(menuItem8);
 
+        // MAINS //
         MenuItem menuItem9 = new MenuItem(restaurant, main, "Prawn Curry", "Delicious prawn curry, chef’s special " +
                 "recipe", 10.00, "https://images.unsplash.com/photo-1559847844-5315695dadae?ixid" +
                 "=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80",
@@ -149,6 +169,7 @@ public class DataLoader implements ApplicationRunner {
         menuItem11.addAllergen(Allergen.CELERY);
         menuItemRepository.save(menuItem11);
 
+        // DESSERTS //
         MenuItem menuItem12 = new MenuItem(restaurant, dessert, "Cheesecake", "This cheesecake will send you to " +
                 "heaven", 6.0, "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixid" +
                 "=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
@@ -177,6 +198,7 @@ public class DataLoader implements ApplicationRunner {
         menuItem14.addAllergen(Allergen.SOYA);
         menuItemRepository.save(menuItem14);
 
+        // DRINKS
         MenuItem menuItem15 = new MenuItem(restaurant, hot_drink, "Espresso", "As dark as your heart", 0.95, "https" +
                 "://i2" +
                 "-prod.mirror.co.uk/incoming/article17203196" +
@@ -190,11 +212,46 @@ public class DataLoader implements ApplicationRunner {
         menuItemRepository.save(menuItem16);
 
         MenuItem menuItem17 = new MenuItem(restaurant, hard_drink, "Tennants", "Yellow garbage juice", 3.5, "data" +
-                ":image/jpeg;base64," +
-                "/9j/4AAQSkZJRgABAQAAAQABAAD" +
-                "/2wCEAAoHCBUVFRUVFRIYGBEYFRgSERgSGBgSGBEYGBgZGRgYGBgcIS4lHB4rHxgYJjgmKy8xNTU1GiQ7QDs0Py40NTEBDAwMEA8QHBISGjQhISExMTQxNDQ0NDQ0NDQ0MTQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NP/AABEIAJ8BPgMBIgACEQEDEQH/xAAcAAACAwEBAQEAAAAAAAAAAAAEBQIDBgEABwj/xABEEAABAwIDBAUICAUBCQAAAAABAAIDBBEFEiEGMUFRImFxgZETIzJiobHB0QcUJFJykqLwQlNjssLhMzRDZHOCk9Lx/8QAGgEAAgMBAQAAAAAAAAAAAAAAAwQAAQIFBv/EADcRAAIBAgMFBAgEBwAAAAAAAAABAgMRBBIhMUFhcbETIlHBBSMzgZGh4fAyNGLRFCRScoKy4v/aAAwDAQACEQMRAD8A3NNuCJsqKXcigEQoGkCrarZQqQ5YZYLXnolYbE3XcVs8Tf0Svn+I1HSIXPxCeZDdJ91h2CWJIWkZDcLJYDJ0ytxTMuEK2tiSYkqqIl+gQtRhDyNAtpHSg8ESKQck5Sp6C8panx/ENnpDc20SyjweXOGNaSSbbl9unoARuQuF4WwPJy6oz0jdA9rBtmtnsjBmN321WlZTAcEbBFYKbmqoq+01sAxTjkqKmnFkzsh6lZqrQ1HafLdpIssnUhoDuTPbK2cdqSwP3LnrVDu4dQuRRF0ugkRrHqWMHREpiNRMi0OEbPmRrXvcWtOrWt3kHcSTu7EWEXJ2QKTSEIaE4w7A3vFz0GcC4ansHzWnpMGhjN2xgu5uOYjsvuTEBMxof1AnPwEVPs5E30i556zlHgPmjWYZC3dEzvGb3phZeIRVCK2IzmYH9Uj/AJbPyN+Sg+kiO+Nn5G/JGOVLyrsirgv1WMbmAdgsuOibwPxVriqXlU0i7sqc23Ee5QIXZHISXS5BsQCR3LBovcFWVb+yq3KyFTwqHhEuVDlZli+qWdrd60VXuWdrN6hBBih0KydU6xWqxU6FYyuf0laKP0VTDRF2QtLuCMRigWZAukTCcJZK3eqRBfiknRK+b4g853L6HiLdCsDiEfTKUrLvB4Pul2zz/OL6VQjQL5vgrLSBfR6B2gSz/Eb3DiFqKY2+iGhdoi4TqE9T2C0gplECNVZFQMabgaqbZAF41LRvcB3ohWhbkCg5oVL65g/jb4hDPxWL+az8wVFhTgFW8DiEC/GYP5zPzt+apdjcH85n5gstovUtqsKp5PThY78TRfxQEmzFId0WX8JcPir3YxD/ADWfmC83EmHc9p7CEOTjw+RtKX3cVVuzMbWEsc4EajMbjsWba+y29XUAsfr/AAlYQpaqknoEg3vOTTaL6bs5JmpoDzjb7l8rqTovpWyUn2SDqZbwJCJh9pioaALyHfVMbvcB2kIZ+MQt3yN7jdNXBDFRulL8ehAvnvbkEE/ayAHe7wHzUckt5dm9iH7iqHuWZn23p28HnsA+aBqPpAgG5jz+ULPaw8S1Tm9xrnuVT3LFSfSFFwjee8BBy/SE2+kBI/EPksOtDxNKlPwNu96Fmdfv0WOO3jD/AMJ3X0hYd9kxwraJksgZkc1weIzcgi+u7q6JQXVVwnZSSNY5QcrCoFMgCpypeEQ5UvChBbVDQrO1e9aOs3LO1O9RkM9ijdCsXiA6S3GKDQrFYiOkoi7H6HozoEYkmG1rS0a8EyFSEcGicoQro1bJUBU/WG81hmkLMTjABWCrwM5W0xqsaGnVfO66sBedUvNXYWLshxhbAHAnethR1TbBfOoMQA4o1uOZRvugSg7hLo+n0tSDxRdTNZjy09INJHgvnGHbSi4uU/OPMcxwzakEJqLtEA1qIJcZqnE3ldbq0UoHyn03uPaSVKnZfgmkEYXKp4lt6nQdCNhTVB9t5SefNYrXVcItuSOoh0Kcda4PsrGaexyrLSmksaHyIDqG1AGaxysjzg6OPcbItjFcyPVAlWNqmMsMqpA1wLyW21BN10zKyCOzHHqVAjutYaWaLfEHXVmekfdGUuLTNibGx5DGk2tpxJ+KBcxWUjdLdaLWllhdA6SUpalhle49JxJ7SiIQT2rrY0TBYG3Yg06oxKmWtjOV3clksBv++a0IZ0XctyBfGExUqWsYhBGWrojcpXLEb/Naqqi36JTURdSXVfUY7HQUCErv1ZGhmqmWBU6zK7JAkNKCdQtLs1St+sxEDW9/Ab/AJVCxaTZaLz7TyDiPylCjNupFcV1JKKVNvgzclRKmold05BWVU9XOVL1ChZWcVn6neVoqzcs7VbyoyCLEzoVisQPSWzxM6FYmv9JRGx9h+2DmAA3TRm3Y43XzxeR7sDY+hu25B3FeZtn6y+eXXLrL1NGyxHaPODqkDqu5ult166rKiXYxFSu/WetLrrwcqyImZjNtVbir4K92ZvSNrj3pNmXWPII7R71WRMvMz63RG9uwJxTJFhzui09QTqFy87TWp2nsJ1LtEmfFZpsDvJ4lNah2iaSVz46eDI612EnQG/ST0Vfawai3JRjtZ8+mY77p8ChCDyPgttUY3Ut3kjldtr+xL5seqDpmB/7QsOK8fl9RhYOs9e78X+xno0XTjVHUuKSSmRryC0RuPogaiyEphqErVVjLpyhLK9oyrXhkDnHqHtSOLEwmG2D8tGSN5c0e1fPGVTwmfRsM9Jv9T8hHFytUtwNrJiQR2DSh4ceuywIqnc1rNjp7tffg5vtB+SLjYWoN8jOGd6nxNQRZxCviYL3sg3SXddFwPSFC7HZLQaU8Ln3a0Eu3gDqXHYTOT/s9OtzR8URhLum8f0pP7CgImuc+wBPOwLvYn8ikle+0zRpueazta276rwKajBptei3vkjH+SWT7PzHf5Ids8Q/yRFYLOcDoQdQRYjuQYge82YwuNrkMBcbc7AbkF0o30vfn9B9Yd5buaty/6KRs9NcC8JubC1RCbngAMyXyxFrnMPpNcWu6iCQfcmdEwieJp0IkYCCLEEPGhCBrnXll/wCpJ/eUOpBJJriCqU+zmo3vpf71ZyFq1OybPOX5B3uWcpgFosKnEfSO46fvwS9KXr4c0BrL1UuRr7qJckRxhv3lF2Mt+8u/2kTj5GPC4Kl7wkrsYb95UyYu37ynaxJkYZWyBZ6qkFyqcQxgWPSWcqcV1OqrPcmWwTicg1WNrvSTaprL8UlqH3K2iAK9ZTyLvk0a4MrsvKzKuZFoq5BeU8q7kUJcrXCFbkXi1US5ALl1PKuFqiZe4+n4bJ0GH1R7k8hdoszhb/Mxn1AnNHLfsXnoLvtcWdlvuoMnfvTwUT5aemyNzWab6gW6XWs7O7RKJqt7QQ2RwA4BxA8An4K20DncZKUdqNptofNxj1nDwaEk2WPnHnlGd/C7mquWQuoqckkkvlJJNyelzQ+FEhlSeUDvehuTlO/DyY9SilgGuf8AsemopGPqHuYWtc1+Um1jd2lkFRnVK21LjoXE9pJTDDn3Pekq92rglNyab4fIu23P2Vo5yNXz8BbrbuS1PEOcnuBWFBTvov8AL+9iGL9r7kTaFo9knf7Ufg/yWcDk/wBlD0pB6rT7T80bGK9CX3vKoe0X3uNKX6phSuSw7wjY5Dua3vJsB8SubQHpmmwXV5FrkskH6CraCmkYZnlj2jyElnWLbG1xY80kjzBwcHuFmkBo01LS0m47fYompcXgFzyCbG7id/en81la3j0t4AYt2lFbJLXaX7R/7xL+P5KvAyQZ7Gx8g6xGlunGrNpD9ol/F8kDRu81WH/lZP7mIc9Zy/y6M6cvyS/tj5BkuGz/AF3P5F/k/rOa+U5cvlL3vysslUG739b3H9RQAlN958SiYyl6ss27x+YtnlNrNuSXw97D4CobS1TmQx5TYul4cgwqym4aITbE9CAes8+AaPil6CviILi+jKru1JiMYlJ94rxxB/3kIF0FdvKjl3C21z/vFT+tv+8UG0rrnLOVeBLkamdx4pZLKb70TOUE9GggcmezlQeV0FcciWMXOKWZVZl260UWXXLqsleuruVYszLt1UCuEq7lWLS5RLlAFcJVXLsTL1ElcuvBQh9Awh16eP8ACmtE5Jdn33p2Dt9hT2lZYXXDStVlzfU622C5ItqX6JXRm88I0IM0QIPEGRosmFYNLpOXlrg9ujmuD2nkWm4PiE89ULM1e0lS973MNgxjnBoaMoA7kpw2peyQBu55DHBwuC0uF1VPtxWc2d8YQrtsqs/xM7o2oTgs2a7+C/cehjkqXZ9mrW8fp468wLGG/aJgNAHkAAWt2IrCPSA60qfUue9z3m7nHM47rk701wg9MJSv+FgqO099IJ83Tj1ifYsY0LXfSE//AHcdTj7lkmvCb9HK2Fj7+rFMQ/WslZOtlj5x4/p/5BJC9Ntl3+eI5xu97UXFL1MuRKHtY8zVE7kVTOQrlfCubQQ/UGYfp3IjA6VkjpC8OOQNc0NdluS4N32PNAF3uRGB4gyJ0nlHOaHtDWlrc1iHB27Tkn0ldX2Cz2Hsanzyvda1zuve1tEHSVTGCQOZnZJGYnNDizQlpOoB+6rqyvoruLqiW5JJtDf/ACQEuJUH8+b/AMA/90Nxle51VicM6fZyby2S2SFe0NM2KofGwEMblIBcXEZmNcbk79XFCwP1XsfxNk1RLIwuyPLMuYAHosa3UcPRKDZMhVIa6HOpz0Vx9TygFo4k7ghNtHWNOPVkd4lo+CjRTDM23BCbcSHysI5QX8Xu+SBQhbEw5PowmIlei+a6igOC5nQglXc67Njm3DmuC8XiyBEi75VRRIdmO9AyFXSOQr3IkUDkTDlElRLlxbMnF5dyruVWQivKVl7KoQivKdl4tUIVleVhaoqEIr1lOy7ZTYQ2Ozbvs46ifetBRzA/FZfAH/Z325lNcKluFysvrZ8zoKXcjyNBLG03GugukFQ3fY8bJy5535uFjpvSepdqdetNNWBXFEoVO7irp+1DEnmFhkQQwbtU4wi178is/nOmqb4G4lwHM6pLER7jGKL7yK/pFf5yEcoifaFjw9ar6Q33qGDlEPesonfR6/lafLzFK+tWRIPKb7MSfaG9bXj2f6JQAmWzzrVMfWXDxaUavrSlyfQqjpUjzRtZJLW7UXDC+2a2lr7xu3oCQa7+KJilHkiC6zi8EC9iRoNFysPqP1XYZOp32vl6uCDnGU2MZu30j+Ldx601c4FrSHDc29jqbcxy4oDEJ2kP6Qzghu8ajMCCOe/3roNWFkzP1o1cMhzBlyRpY39I69SXSREC5iJsBrppYm538RZOMRqGOEozDMA5gNxqCARb98Evr3Nd6L2aDUh4ufVsDuQd4RgUkZ1Ajyn0ju3bh7Va2lfxb42V01Q0h4uMw0FjvBsdP3wUg8ERgOG7XW/ihybSLSVyyhgcHi7d6B25P2po+7BEPHMfitFSOBe2xHXZINr23qn9TY2/oafihYRuWJ13RfVGsSlGlbiZwBdRBjVbmLsHPuVFeK4SvEqFkHlUOVr1U4K0YZwLl1IMK7lWzJ4uXsy4VyyhRLMuAroauhqhZy66CuhillUKK3FRCsLF3KqTIV3XrqzKuhqu5Zo9mjeGQet8EfhpINuSB2WPm5h1g+xGUxu487rnL21RcfIbv6uL4Ggcegk0796a72HsSGof6XUnJx0QFS1BnoZ5AUH1BKHe8lL5QuYvMoT3Z1wLwsw5hT7ZJp8oBwQMVFKi2boy76BNvJL1VuTGD2LOgp1tm69XJ1ZR7EjCawith6a/SugCo71JcyxpR2CutURH+oPbol4ReHPtLEfXZ/cESorwkuD6Eg7ST4rqbqr4dq9E/WxaDbnf4Fcqxr3r1NHc6hcnCvRMfrbWPKZwyjQc+fdqk9a5oPo3Ou/UagdXUfEp1SRHKlVc03NmtGtr6X07dV05rRCcXtM5VVTczh5IDUnf1W5d/ago5BYARAkWuRfXW+uncj66ZmY3y3tlA0+AOqWOlbrYe91vaFhIu5Y2S38AHbYcevwV8U+t8o/d+SBY8Hhp1Wb8ETC3UW5oVRI1Btj7CJbu7+Gm6yTbSyfap+ohvg1g+Ce7OUwJ1OuttN3LtWaxxhdU1B/qvHgbfBBwNv4mXCPmjWLb7KPPyAy8KBN118JCiwWXWkkc9Msip7ottCOSjTzAK8VY5LFgmYpfQBUOogi3VQK4ZwrsymwF1LZRNMjS8KOcK8rJcTgLuVWmKy9kKu5ixWAukKXkl3IoS5DKu5VaGKWVSxLlGVdLFabLihVynya5kV2YKJcFCD3ZgdGUdQ+KNpm9M9qE2T6TpR6gPtKPhHTPaue9MRNcug5H2Uff1HQb5srP1OuZaOMdA9izFa+xcF0JLuoAtrFrmi6sjaBwVRcpNelJoNFk3sun2yrbPSJz072YBMjUlivYyDwdpJmZ2mfeqmPr28AEsR2Pm9TN+N3vS8Lp0ValBcF0E5Pvvm+pMK2mdZzPxNPtCouptdYg9d0S19C7n0auZcqNK43sf/iKqm7j1A+xCyOyAuI+F1w8K9EdKttNFTO6CzmJk5gevVPMMfmjDrbx4LP4g7pk3Nl2JLuI56/EzOVvScT1oF+8phVgZiUG8AlDLsVxJnQvBPYl8bCSmdBHru6kGu1YJSvc1OAN6TR1j/VIKqMGWU85Hn9blqcBiGZvPRY0ykucebifE3S/oxKVao+EfP8AY1jtFBczk8QslUwsUzmcbJZMuvIQRWxxVl1WxSJWTR5zlEPUHOXmlWVYtzFdXmrt1LksRkcLqcbAgnOJKJjfYLO40SlNkMZV6oJO5VNpXnh7QtxWhhlhmUDP1qxuHv8A2QpjDHcfetXRVgV0655ZMo8L5o+nwpvFZzIuwgzOPBSZTvO5pWypcNjHBOaajj+6qzM1lM3sZSPZI8uFgY7a9qbeT6S0EEDbOyixylZmzg8XOl9QubV0xDfikNU16u3Eews6B7FjcQYc7uvct5TRebf+G4WKxBnSPtXRb7kWAt3mKGgq1rdVc0KbGJScw0YnGxXWl2Ygs6/LX2JPFGtPs/HZxPqn3Lm4qpeDiMQVtT5lizrzzH+q73oRE1zXF8hPGR5/UUMQu7BWikc/edC8VxdI0Wt5D6i/VjDzYw+LQhZo84Ld246I2mAdTxHiYmEflCGZJxI4Lz+HerXFnVqaq44wtmWO19wSLEmdIrQ4Xqw+CSYm3pG3P4rtSdqcRBLvMy1YzXvQD02qmakdqUzN1Qk7mmrE6c6p5hLAXW4LPxGyYUVVkdfs3IGIg5J2N0pJPU+h4ZC1oJG8NJ/SvndMbgdi2+B1vlI5fVie7X8KxNM3QBY9ERcXVzfp8zOOkm428GSkCAmCauj0QMzN67EkJIXk6r116ULjUM0ccuNK69qruqLLi9czKkuXQ9WiH//Z",
+                "https://www.telegraph.co.uk/content/dam/men/2016/05/13/tennents-588470_trans_NvBQzQNjv4Bq2mG3lyVb0qwi9Nuwg-8hyWM4S1u_kNqQtETCuvfEn_0.jpg",
                 false, true, true, true, "pint", 4);
         menuItemRepository.save(menuItem17);
+
+        restaurant.addMenuItem(menuItem1);
+        restaurant.addMenuItem(menuItem2);
+        restaurant.addMenuItem(menuItem3);
+        restaurant.addMenuItem(menuItem4);
+        restaurant.addMenuItem(menuItem5);
+        restaurant.addMenuItem(menuItem6);
+        restaurant.addMenuItem(menuItem7);
+        restaurant.addMenuItem(menuItem8);
+        restaurant.addMenuItem(menuItem9);
+        restaurant.addMenuItem(menuItem10);
+        restaurant.addMenuItem(menuItem11);
+        restaurant.addMenuItem(menuItem12);
+        restaurant.addMenuItem(menuItem13);
+        restaurant.addMenuItem(menuItem14);
+        restaurant.addMenuItem(menuItem15);
+        restaurant.addMenuItem(menuItem16);
+        restaurant.addMenuItem(menuItem17);
+        restaurantRepository.save(restaurant);
+
+        // Setup orders
+        Order order1 = new Order(renee);
+        order1.addItemToOrder(menuItem1);
+        order1.addItemToOrder(menuItem2);
+        orderRepository.save(order1);
+
+        // Setup tables
+        Table table1 = new Table(restaurant, 1, 4);
+        Table table2 = new Table(restaurant, 2, 2);
+        table2.addCustomer(renee);
+        table2.addCustomer(reneesHusband);
+        tableRepository.save(table1);
+        tableRepository.save(table2);
+
+        restaurant.addTable(table1);
+        restaurant.addTable(table2);
+        restaurantRepository.save(restaurant);
 
     }
 
