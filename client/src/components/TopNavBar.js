8bf1b7f3cf15584b8300f7f0bae457c5eb6af546
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-04-13 11:41:39 +0100

    scott fixed hamburger bug

--- client/src/components/TopNavBar.js
+++ client/src/components/TopNavBar.js
@@ -1,26 +1,9 @@
-import FooDeeLogo from './FooDeeLogo'
-import HamburgerLogo from './HamburgerLogo';
-import HomeLogo from './HomeButtonLogo';
-import BasketLogo from './BasketLogo';
 import HamburgerMenu from './HamburgerMenu';
 
 const TopNavBar = ({ basketCounter }) => {
     return (
         <>
-        
-        <div className="top-navbar-container-all">
-            <div className="top-navbar-container-left">
-                {/* <FooDeeLogo></FooDeeLogo> */}
-            </div>
-            <div className="top-navbar-container-right">
-                {/* <HomeLogo></HomeLogo>
-                <BasketLogo></BasketLogo> */}
-                {/* <HamburgerLogo></HamburgerLogo> */}
-                
-            </div>
-        </div>
         <HamburgerMenu></HamburgerMenu>
-        {/* <HamburgerMenu></HamburgerMenu> */}
         </>
     )
 
