96b8884f180a919e9064ae61861c66464b5f023e
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-04-13 17:05:08 +0100

    final merger before the next merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './css/App.css';
 import './css/base.css'
-import './css/TopNavBar.css'
-import './css/MenuNavBar.css'
-import './css/animations.css'
+import './css/TopNavBar.css';
+import './css/MenuNavBar.css';
+import './css/animations.css';
 import MainContainer from './containers/MainContainer';
 
 function App() {
