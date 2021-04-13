a41a8d61e63bcc07efaa7d61708c7c6c4712c334
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-04-13 08:43:47 +0100

    order form conflict

--- client/src/components/OrderForm.js
+++ client/src/components/OrderForm.js
@@ -36,33 +36,6 @@ const OrderForm = ({ customer, basket, basketValue, handleOrderPost }) => {
     }
 
     return (
-<<<<<<< HEAD
-        <>
-             <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="special notes" name="specialNotes" onChange={handleChange} value={stateOrder.specialNotes} />
-                {/* <select name="table" onChange={handleTable} defaultValue="select-table">
-        <option disabled value='select-ship'>Select a Table</option>
-            {tableOptions}
-        </select>  */}
-           
-            </form>
-            <Link to="/paymentform" className="checkout-text" basket={basket}>
-            <button className="order-btn" type="submit">Pay Now</button>
-            </Link>
-        </>
-
-        //         <i className="fas fa-shopping-cart"></i>
-        // <div className="basket-item-count">{qtyInBasket}</div>
-        // <button onClick={handlePayment}>
-        //     <Link to="/paymentform" className="checkout-text">
-        //     CHECKOUT
-        //     </Link>
-        // </button>
-        // <div className="checkout-total">£{basketValue}</div>
-
-
-
-=======
         <div className="order-form">
             <form onSubmit={handleSubmit}>
                 <textarea className="large-text" placeholder="Special notes..." name="specialNotes" onChange={handleChange} value={stateOrder.specialNotes} />
@@ -72,8 +45,6 @@ const OrderForm = ({ customer, basket, basketValue, handleOrderPost }) => {
                     <button className="order-btn" type="submit">SUBMIT</button>
                 </Link>
             </form>
->>>>>>> develop
-
         </div>
     )
 }
