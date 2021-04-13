dd7d495e48e3a73ccc3ea1dd580f76762d199f17
Laureline Vaucoy <laureline.vaucoy@gmail.com>
2021-04-13 14:53:01 +0100

    drinks sub bar removed but names don;t fit

--- client/src/components/MenuCategoryNavBar.js
+++ client/src/components/MenuCategoryNavBar.js
@@ -21,7 +21,13 @@ const MenuCategoryNavBar = ({ onCategoryNavClick }) => {
                     <Link to="/menu" onClick={() => { onCategoryNavClick("menu_items/category/child_meal") }}>Kids</Link>
                 </li>
                 <li className="category-nav-link" >
-                    <Link to="/menu" onClick={() => { onCategoryNavClick("menu_items/category/hard_drink") }}>Drinks</Link>
+                    <Link to="/menu" onClick={() => { onCategoryNavClick("menu_items/category/hard_drink") }}>Beers {'&'} Wines</Link>
+                </li>
+                <li className="category-nav-link" >
+                    <Link to="/menu" onClick={() => { onCategoryNavClick("menu_items/category/soft_drink") }}>Soft Drinks</Link>
+                </li>
+                <li className="category-nav-link" >
+                    <Link to="/menu" onClick={() => { onCategoryNavClick("menu_items/category/hot_drink") }}>Hot Drinks</Link>
                 </li>
             </ul>
         </div >
