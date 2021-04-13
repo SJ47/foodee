62f578b8ad20fe67e610ba7b5e5e6b52cfade3ad
Renee Graham <renee.noluthando@gmail.com>
2021-04-13 15:56:43 +0100

    about page basics sorted - needs more styling

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,6 +1,8 @@
 import AboutMap from './AboutMap';
 import TopNavBar from '../components/TopNavBar';
 import '../css/AboutPage.css';
+import Image1 from "../images/Image1.png";
+
 
 const AboutPage = ({restaurants}) => {
     
@@ -8,9 +10,31 @@ const AboutPage = ({restaurants}) => {
         <>
         <TopNavBar />
         <div className="about-page">
-            <p>
-            Bringing you blablabla and some of the best blabla in the city, this Glasgow restaurant is a must visit if you're looking for something a little different. Make sure to swing by!
-            </p>
+             <h1 className="about-headline"> About us </h1> 
+             <h3>Our address</h3> 
+             <p>Glasgow</p>
+             <p>lockwise, 77 Renfrew St</p>
+             <p>G2 3BZ</p>
+             <p>Call: 0131 290 2600</p>
+
+             <h2> The story behind Boule au Mur </h2> 
+             <span></span>
+             <div className="wrapper"></div>
+          
+            <img className="brothers-image"src={Image1} height="100" width="70"/>
+
+            <p> In 1882 two brothers were playing the popular French game 
+                called Boules when one of their balls made a dent in the wall of 
+                the restaurant they were renovating. They had not decided on a name 
+                for their new establishment, but decided there and then that the name would be 
+                Boule au Mur, which means “balls to the wall”. This would combine their love of the
+                 ball game with their passion for food.  They opened the restaurant in a small French 
+                 village and it was a roaring success. The brothers franchised their business and the love
+                 of good food they brought to their menus is still found in Boule au Mur restaurants around
+                the globe. </p>
+                <span></span>
+
+                <h4>Find other Boule au Mur restuarants</h4>
             <AboutMap restaurants={restaurants} />
         </div>
         </>
