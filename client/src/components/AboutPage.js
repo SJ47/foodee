d888d79603a787e5ce8382a80741174b95911ecc
Tom Pies <tombo@Toms-MacBook-Pro.local>
2021-04-14 11:49:43 +0100

    final final final

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -18,7 +18,7 @@ const AboutPage = ({restaurants}) => {
              <p>G2 3BZ</p>
              <p>Call: 0131 290 2600</p> */}
 
-             <h2> The story behind Boule au Mur </h2> 
+             <h2> The story behind Boules au Mur </h2> 
              {/* <span></span>
              <div className="wrapper"></div> */}
           
@@ -27,15 +27,15 @@ const AboutPage = ({restaurants}) => {
             <p> In 1882 two brothers were playing the popular French game 
                 called Boules when one of their balls made a dent in the wall of 
                 the restaurant they were renovating.<br></br><br></br>They had not decided on a name 
-                for their new establishment, but decided there and then that the name would be <i>Boule au Mur</i>, which means “balls to the wall”.<br></br><br></br>This would combine their love of the
+                for their new establishment, but decided there and then that the name would be <i>Boules au Mur</i>, which means “balls to the wall”.<br></br><br></br>This would combine their love of the
                  ball game with their passion for food.<br></br><br></br>They opened the restaurant in a small French 
                  village, Asnières-sur-Nouère, and it was a roaring success. The brothers franchised their business and the love
-                 of good food they brought to their menus is still found in Boule au Mur restaurants around
+                 of good food they brought to their menus is still found in Boules au Mur restaurants around
                 the globe. </p>
                 {/* <span></span> */}
    
 
-                <h4>Find a Boule au Mur near you</h4>
+                <h4>Find a Boules au Mur near you</h4>
             <AboutMap restaurants={restaurants} />
         </div>
         </>
